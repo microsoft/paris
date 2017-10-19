4aac23f0af0001db55a9f37d478b701bf16632bf
Yossi Kolesnicov <yokolesn@microsoft.com>
2017-10-19 16:19:24 +0300

    Making it work through NPM...

---
+++ definitions/index.d.ts
@@ -0,0 +1,9 @@
+export { DataTransformersService, DataTransformer } from "./lib/services/data-transformers.service";
+export { ModelEntity, EntityConfig, ModelEntityCacheConfig } from "./lib/entity/entity.config";
+export { entityFieldsService } from "./lib/services/entity-fields.service";
+export { entitiesService } from "./lib/services/entities.service";
+export { ModelObjectValue } from "./lib/entity/object-value.config";
+export { EntityField } from "./lib/entity/entity-field.decorator";
+export { ObjectValue } from "./lib/entity/object-value.decorator";
+export { Entity } from "./lib/entity/entity.decorator";
+export { ParisModule } from "./lib/paris.module";
