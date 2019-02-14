67609c0805d0bdc8dc85b90dba4dac4f105715fa
gibarila <gibarila@microsoft.com>
2019-02-14 16:16:08 +0200

    typo

--- lib/config/paris-config.ts
+++ lib/config/paris-config.ts
@@ -1,12 +1,11 @@
 import { AjaxRequest, AjaxResponse } from "rxjs/ajax";
 import { Observable } from "rxjs";
-import { ParisConfig } from "../main";
 
 export interface AjaxService {
 	(urlOrRequest: string | AjaxRequest): Observable<AjaxResponse>;
 }
 
-export interface ParisConfigParisConfig<TConfigData = any> {
+export interface ParisConfig<TConfigData = any> {
 	apiRoot?: string,
 	allItemsProperty?: string,
 	entityIdProperty?: string,
